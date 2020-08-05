229d39cb72537e9e381b95045dd812b935eca64a
catesancarlos <catesancarlosricaurte@gmail.com>
2020-08-05 12:13:29 -0500

    spinner 1

--- pages/miConfirmacion.js
+++ pages/miConfirmacion.js
@@ -1,9 +1,17 @@
 import {useEffect} from 'react'
-import { useRouter } from 'next/router'
+import Router, { useRouter } from 'next/router'
 import Head from 'next/head'
 import {IoIosCheckmarkCircle} from 'react-icons/io'
 import Header from '../componentes/layout/header'
 import Footer from '../componentes/layout/footer2'
+
+import NProgress from 'nprogress'
+
+Router.onRouteChangeStart = (url) => {
+    NProgress.start()
+}
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 const MiConfirmacion = () => {
     const router = useRouter()
@@ -295,6 +303,83 @@ const MiConfirmacion = () => {
                 p{
                     margin: 0;
                 }
+
+
+
+                /* Make clicks pass-through */
+                #nprogress {
+                pointer-events: none;
+                }
+
+                #nprogress .bar {
+                background: brown;
+
+                position: fixed;
+                z-index: 1031;
+                top: 0;
+                left: 0;
+
+                width: 100%;
+                height: 5px;
+                }
+
+                /* Fancy blur effect */
+                #nprogress .peg {
+                display: block;
+                position: absolute;
+                right: 0px;
+                width: 100px;
+                height: 100%;
+                box-shadow: 0 0 10px brown, 0 0 5px brown;
+                opacity: 1.0;
+
+                -webkit-transform: rotate(3deg) translate(0px, -4px);
+                    -ms-transform: rotate(3deg) translate(0px, -4px);
+                        transform: rotate(3deg) translate(0px, -4px);
+                }
+
+                /* Remove these to get rid of the spinner */
+                #nprogress .spinner {
+                display: block;
+                position: fixed;
+                z-index: 1031;
+                top: 15px;
+                right: 15px;
+                }
+
+                #nprogress .spinner-icon {
+                width: 20px;
+                height: 20px;
+                box-sizing: border-box;
+
+                border: solid 3px transparent;
+                border-top-color: brown;
+                border-left-color: brown;
+                border-radius: 50%;
+
+                -webkit-animation: nprogress-spinner 400ms linear infinite;
+                        animation: nprogress-spinner 400ms linear infinite;
+                }
+
+                .nprogress-custom-parent {
+                overflow: hidden;
+                position: relative;
+                }
+
+                .nprogress-custom-parent #nprogress .spinner,
+                .nprogress-custom-parent #nprogress .bar {
+                position: absolute;
+                }
+
+                @-webkit-keyframes nprogress-spinner {
+                0%   { -webkit-transform: rotate(0deg); }
+                100% { -webkit-transform: rotate(360deg); }
+                }
+                @keyframes nprogress-spinner {
+                0%   { transform: rotate(0deg); }
+                100% { transform: rotate(360deg); }
+                }
+
             `}</style>
         </div> 
     )
