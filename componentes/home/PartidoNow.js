f9044c435487958ed8469289ea6b95f22ac1a48d
paasmal <pedroasmal@hotmail.com>
2025-03-14 11:36:38 -0500

    version 1.0.41

--- componentes/home/PartidoNow.js
+++ componentes/home/PartidoNow.js
@@ -12,15 +12,15 @@ export default function PartidoNow({ fecha, home = true }) {
                     pen={fecha?.pen1}
                     home={home}
                     dia='Sábado'
-                    fecha='08 Mar'
-                    hora='17h10'
-                    genero='F'
-                    equipos={['Santa Fé', 'Las Angeles Negros']}
-                    paralelos={['1ro Confir. 2 (F)', '1ro Confir. 3 (F)']}
-                    logos={['12F', '13F']}
+                    fecha='15 Mar'
+                    hora='10h30'
+                    genero='M'
+                    equipos={['Niños gerreros de Dios', 'Los elegidos de Dios']}
+                    paralelos={['Iniciación 1 (M)', 'Iniciación 2 (M)']}
+                    logos={['I1M', 'I2M']}
                     colores={{
-                        eq1: ['black', 'black', 'white'],
-                        eq2: ['black', 'black', 'white']
+                        eq1: ['white', 'black', 'black'],
+                        eq2: ['white', 'black', 'black']
                     }}
                 />
             }
@@ -31,15 +31,15 @@ export default function PartidoNow({ fecha, home = true }) {
                     jugador={fecha?.jugador2}
                     pen={fecha?.pen2}
                     home={home}
-                    dia='Domingo'
-                    fecha='09 Mar'
-                    hora='08h30'
-                    genero='M'
-                    equipos={['Santa Fé', 'Newcastels']}
-                    paralelos={['1ro Confir. 2 (M)', '1ro Confir. 4 (M)']}
-                    logos={['12M', '14M']}
+                    dia='Sábado'
+                    fecha='15 Mar'
+                    hora='11h40'
+                    genero='F'
+                    equipos={['Mensajeras de Dios', 'Las Campeonas del Futbol']}
+                    paralelos={['1ro Comun. 1 (F)', '1ro Comun. 2 (F)']}
+                    logos={['11F', '12F']}
                     colores={{
-                        eq1: ['black', 'black', 'white'],
+                        eq1: ['white', 'black', 'black'],
                         eq2: ['white', 'black', 'black']
                     }}
                 />
@@ -51,16 +51,16 @@ export default function PartidoNow({ fecha, home = true }) {
                     jugador={fecha?.jugador3}
                     pen={fecha?.pen3}
                     home={home}
-                    dia='Domingo'
-                    fecha='09 Mar'
-                    hora='09h20'
-                    genero='F'
-                    equipos={['Confirmación F.C.', 'Génesis']}
-                    paralelos={['2do Confir. 2 (F)', '2do Confir. 3 (F)']}
-                    logos={['22F', '23F']}
+                    dia='Sábado'
+                    fecha='15 Mar'
+                    hora='12h10'
+                    genero='M'
+                    equipos={['Mensajeros de Dios', 'Los Campeones del Futbol']}
+                    paralelos={['1ro Comun. 1 (M)', '1ro Comun. 2 (M)']}
+                    logos={['11M', '12M']}
                     colores={{
-                            eq1: ['black', 'black', 'white'],
-                            eq2: ['white', 'black', 'black']
+                        eq1: ['white', 'black', 'black'],
+                        eq2: ['white', 'black', 'black']
                     }}
                 />
             }
@@ -71,16 +71,16 @@ export default function PartidoNow({ fecha, home = true }) {
                     jugador={fecha?.jugador4}
                     pen={fecha?.pen4}
                     home={home}
-                    dia='Domingo'
-                    fecha='09 Mar'
-                    hora='10h20'
-                    genero='F'
-                    equipos={['Camino a la Tierra Prometída', 'Mujeres de Nazareth']}
-                    paralelos={['Año Bíblico 5 (F)', 'Año Bíblico 4 (F)']}
-                    logos={['A5F', 'A4F']}
+                    dia='Sábado'
+                    fecha='15 Mar'
+                    hora='16h40'
+                    genero='M'
+                    equipos={['Peruanitos de Dios', 'Los Águilas']}
+                    paralelos={['Conf. Molinopamba (M)', '1ro Confir. 1 (M)']}
+                    logos={['2MM', '11M']}
                     colores={{
                         eq1: ['white', 'black', 'black'],
-                        eq2: ['red', 'red', 'white']
+                        eq2: ['white', 'black', 'black']
                     }}
                 />
             }
@@ -91,15 +91,15 @@ export default function PartidoNow({ fecha, home = true }) {
                     jugador={fecha?.jugador5}
                     pen={fecha?.pen5}
                     home={home}
-                    dia='Domingo'
-                    fecha='09 Mar'
-                    hora='11h20'
-                    genero='F'
-                    equipos={['Las Misioneras', 'Las Herederas de Dios']}
-                    paralelos={['Año Bíblico 2 (F)', 'Año Bíblico 1 (F)']}
-                    logos={['A2F', 'A1F']}
+                    dia='Sábado'
+                    fecha='15 Mar'
+                    hora='17h20'
+                    genero='M'
+                    equipos={['Traicioneros como Judas', 'Juntos con Cristo FC']}
+                    paralelos={['2do Confir. 5 (M)', '1 Conf. AB Ángeles (M)']}
+                    logos={['25M', '1AM']}
                     colores={{
-                        eq1: ['white', 'black', 'black'],
+                        eq1: ['black', 'black', 'white'],
                         eq2: ['white', 'black', 'black']
                     }}
                 />
@@ -112,14 +112,114 @@ export default function PartidoNow({ fecha, home = true }) {
                     pen={fecha?.pen6}
                     home={home}
                     dia='Domingo'
-                    fecha='09 Mar'
-                    hora='12h20'
-                    genero='M'
-                    equipos={['Los Pastores', 'Santos y Religiosos F.C.']}
-                    paralelos={['2do Confir. 3 (M)', '2do Confir. 1 (M)']}
-                    logos={['23M', '21M']}
+                    fecha='16 Mar'
+                    hora='08h30'
+                    genero='F'
+                    equipos={['Confirmación F.C.', 'Las Galatas 1, 10']}
+                    paralelos={['2do Confir. 2 (F)', '2do Confir. 1 (F)']}
+                    logos={['22F', '21F']}
                     colores={{
-                        eq1: ['blue', 'blue', 'white'],
+                        eq1: ['black', 'black', 'white'],
+                        eq2: ['white', 'black', 'black']
+                    }}
+                />
+            }
+            { fecha?.partido7 == 1 &&
+                <ItemCalendario
+                    now={fecha?.partido7}
+                    res={fecha?.res7}
+                    jugador={fecha?.jugador7}
+                    pen={fecha?.pen7}
+                    home={home}
+                    dia='Domingo'
+                    fecha='16 Mar'
+                    hora='09h20'
+                    genero='F'
+                    equipos={['Traicioneras como Judas', 'Estrellitas de Maria']}
+                    paralelos={['2do Confir. 5 (F)', '2 Conf. Ángeles (F)']}
+                    logos={['25F', '2AF']}
+                    colores={{
+                        eq1: ['black', 'black', 'white'],
+                        eq2: ['black', 'black', 'white']
+                    }}
+                />
+            }
+            { fecha?.partido8 == 1 &&
+                <ItemCalendario
+                    now={fecha?.partido8}
+                    res={fecha?.res8}
+                    jugador={fecha?.jugador8}
+                    pen={fecha?.pen8}
+                    home={home}
+                    dia='Domingo'
+                    fecha='16 Mar'
+                    hora='10h10'
+                    genero='F'
+                    equipos={['Santa Fé', 'Las Águilas']}
+                    paralelos={['1ro Confir. 2 (F)', '1ro Confir. 1 (F)']}
+                    logos={['12F', '11F']}
+                    colores={{
+                        eq1: ['black', 'black', 'white'],
+                        eq2: ['white', 'black', 'black']
+                    }}
+                />
+            }
+            { fecha?.partido9 == 1 &&
+                <ItemCalendario
+                    now={fecha?.partido9}
+                    res={fecha?.res9}
+                    jugador={fecha?.jugador9}
+                    pen={fecha?.pen9}
+                    home={home}
+                    dia='Domingo'
+                    fecha='16 Mar'
+                    hora='11h00'
+                    genero='F'
+                    equipos={['Club GNU', 'Newcastells']}
+                    paralelos={['2do Confir. 4 (F)', '1ro Confir. 4 (F)']}
+                    logos={['24F', '14F']}
+                    colores={{
+                        eq1: ['black', 'black', 'white'],
+                        eq2: ['white', 'black', 'black']
+                    }}
+                />
+            }
+            { fecha?.partido10 == 1 &&
+                <ItemCalendario
+                    now={fecha?.partido10}
+                    res={fecha?.res10}
+                    jugador={fecha?.jugador10}
+                    pen={fecha?.pen10}
+                    home={home}
+                    dia='Domingo'
+                    fecha='16 Mar'
+                    hora='11h50'
+                    genero='F'
+                    equipos={['Génesis', 'Las Ángeles Negros']}
+                    paralelos={['2do Confir. 3 (F)', '1ro Confir. 3 (F)']}
+                    logos={['23F', '13F']}
+                    colores={{
+                        eq1: ['white', 'black', 'black'],
+                        eq2: ['black', 'black', 'white']
+                    }}
+                />
+            }
+            { fecha?.partido11 == 1 &&
+                <ItemCalendario
+                    now={fecha?.partido11}
+                    res={fecha?.res11}
+                    jugador={fecha?.jugador11}
+                    pen={fecha?.pen11}
+                    home={home}
+                    dia='Domingo'
+                    fecha='16 Mar'
+                    hora='12h30'
+                    genero='M'
+                    equipos={['Los Angelicales', 'Newcastells']}
+                    paralelos={['AB Conf. Molinopamba (M)', '1ro Confir. 4 (M)']}
+                    logos={['1MM', '14M']}
+                    colores={{
+                        eq1: ['white', 'black', 'black'],
                         eq2: ['white', 'black', 'black']
                     }}
                 />
--- pages/paasmal.js
+++ pages/paasmal.js
@@ -12,7 +12,7 @@ export default function Paasmal(){
 
     useEffect(() => {
         if(log){
-            onSnapshot(doc(db, 'campeonato25', 'fecha7'), (doc) => {
+            onSnapshot(doc(db, 'campeonato25', 'fecha8'), (doc) => {
                 setFecha(doc.data())
             }) 
         }
@@ -24,88 +24,160 @@ export default function Paasmal(){
 
     const handleStatus = async e => {
         if(e[0]==1){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { partido1: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido1: e[1] })
         }
         if(e[0]==2){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { partido2: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido2: e[1] })
         }
         if(e[0]==3){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { partido3: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido3: e[1] })
         }
         if(e[0]==4){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { partido4: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido4: e[1] })
         }
         if(e[0]==5){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { partido5: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido5: e[1] })
         }
         if(e[0]==6){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { partido6: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido6: e[1] })
+        }
+        if(e[0]==7){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido7: e[1] })
+        }
+        if(e[0]==8){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido8: e[1] })
+        }
+        if(e[0]==9){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido9: e[1] })
+        }
+        if(e[0]==10){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido10: e[1] })
+        }
+        if(e[0]==11){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { partido11: e[1] })
         }
     }
     
     const handleGoles = async e => {
         if(e[0]==1){
             if(e[1]==0){  
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res1: [e[2]=='+' ? fecha.res1[0]+1 : fecha.res1[0]-1, +fecha.res1[1]] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res1: [e[2]=='+' ? fecha.res1[0]+1 : fecha.res1[0]-1, +fecha.res1[1]] })
             } else{
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res1: [+fecha.res1[0], e[2]=='+' ? fecha.res1[1]+1 : fecha.res1[1]-1] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res1: [+fecha.res1[0], e[2]=='+' ? fecha.res1[1]+1 : fecha.res1[1]-1] })
             }
         }
         if(e[0]==2){
             if(e[1]==0){  
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res2: [e[2]=='+' ? fecha.res2[0]+1 : fecha.res2[0]-1, +fecha.res2[1]] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res2: [e[2]=='+' ? fecha.res2[0]+1 : fecha.res2[0]-1, +fecha.res2[1]] })
             } else{
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res2: [+fecha.res2[0], e[2]=='+' ? fecha.res2[1]+1 : fecha.res2[1]-1] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res2: [+fecha.res2[0], e[2]=='+' ? fecha.res2[1]+1 : fecha.res2[1]-1] })
             }
         }
         if(e[0]==3){
             if(e[1]==0){  
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res3: [e[2]=='+' ? fecha.res3[0]+1 : fecha.res3[0]-1, +fecha.res3[1]] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res3: [e[2]=='+' ? fecha.res3[0]+1 : fecha.res3[0]-1, +fecha.res3[1]] })
             } else{
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res3: [+fecha.res3[0], e[2]=='+' ? fecha.res3[1]+1 : fecha.res3[1]-1] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res3: [+fecha.res3[0], e[2]=='+' ? fecha.res3[1]+1 : fecha.res3[1]-1] })
             }
         }
         if(e[0]==4){
             if(e[1]==0){  
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res4: [e[2]=='+' ? fecha.res4[0]+1 : fecha.res4[0]-1, +fecha.res4[1]] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res4: [e[2]=='+' ? fecha.res4[0]+1 : fecha.res4[0]-1, +fecha.res4[1]] })
             } else{
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res4: [+fecha.res4[0], e[2]=='+' ? fecha.res4[1]+1 : fecha.res4[1]-1] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res4: [+fecha.res4[0], e[2]=='+' ? fecha.res4[1]+1 : fecha.res4[1]-1] })
             }
         }
         if(e[0]==5){
             if(e[1]==0){  
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res5: [e[2]=='+' ? fecha.res5[0]+1 : fecha.res5[0]-1, +fecha.res5[1]] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res5: [e[2]=='+' ? fecha.res5[0]+1 : fecha.res5[0]-1, +fecha.res5[1]] })
             } else{
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res5: [+fecha.res5[0], e[2]=='+' ? fecha.res5[1]+1 : fecha.res5[1]-1] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res5: [+fecha.res5[0], e[2]=='+' ? fecha.res5[1]+1 : fecha.res5[1]-1] })
             }
         }
         if(e[0]==6){
             if(e[1]==0){  
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res6: [e[2]=='+' ? fecha.res6[0]+1 : fecha.res6[0]-1, +fecha.res6[1]] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res6: [e[2]=='+' ? fecha.res6[0]+1 : fecha.res6[0]-1, +fecha.res6[1]] })
             } else{
-                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { res6: [+fecha.res6[0], e[2]=='+' ? fecha.res6[1]+1 : fecha.res6[1]-1] })
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res6: [+fecha.res6[0], e[2]=='+' ? fecha.res6[1]+1 : fecha.res6[1]-1] })
+            }
+        }
+        if(e[0]==7){
+            if(e[1]==0){  
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res7: [e[2]=='+' ? fecha.res7[0]+1 : fecha.res7[0]-1, +fecha.res7[1]] })
+            } else{
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res7: [+fecha.res7[0], e[2]=='+' ? fecha.res7[1]+1 : fecha.res7[1]-1] })
+            }
+        }
+        if(e[0]==8){
+            if(e[1]==0){  
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res8: [e[2]=='+' ? fecha.res8[0]+1 : fecha.res8[0]-1, +fecha.res8[1]] })
+            } else{
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res8: [+fecha.res8[0], e[2]=='+' ? fecha.res8[1]+1 : fecha.res8[1]-1] })
+            }
+        }
+        if(e[0]==9){
+            if(e[1]==0){  
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res9: [e[2]=='+' ? fecha.res9[0]+1 : fecha.res9[0]-1, +fecha.res9[1]] })
+            } else{
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res9: [+fecha.res9[0], e[2]=='+' ? fecha.res9[1]+1 : fecha.res9[1]-1] })
+            }
+        }
+        if(e[0]==10){
+            if(e[1]==0){  
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res10: [e[2]=='+' ? fecha.res10[0]+1 : fecha.res10[0]-1, +fecha.res10[1]] })
+            } else{
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res10: [+fecha.res10[0], e[2]=='+' ? fecha.res10[1]+1 : fecha.res10[1]-1] })
+            }
+        }
+        if(e[0]==7){
+            if(e[1]==0){  
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res7: [e[2]=='+' ? fecha.res7[0]+1 : fecha.res7[0]-1, +fecha.res7[1]] })
+            } else{
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res7: [+fecha.res7[0], e[2]=='+' ? fecha.res7[1]+1 : fecha.res7[1]-1] })
+            }
+        }
+        if(e[0]==11){
+            if(e[1]==0){  
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res11: [e[2]=='+' ? fecha.res11[0]+1 : fecha.res11[0]-1, +fecha.res11[1]] })
+            } else{
+                const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { res11: [+fecha.res11[0], e[2]=='+' ? fecha.res11[1]+1 : fecha.res11[1]-1] })
             }
         }
     }
 
     const handleAgregar = async e => {
         if(e[0]==1){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { jugador1: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador1: e[1] })
         }
         if(e[0]==2){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { jugador2: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador2: e[1] })
         }
         if(e[0]==3){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { jugador3: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador3: e[1] })
         }
         if(e[0]==4){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { jugador4: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador4: e[1] })
         }
         if(e[0]==5){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { jugador5: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador5: e[1] })
         }
         if(e[0]==6){
-            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha7'), { jugador6: e[1] })
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador6: e[1] })
+        }
+        if(e[0]==7){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador7: e[1] })
+        }
+        if(e[0]==8){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador8: e[1] })
+        }
+        if(e[0]==9){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador9: e[1] })
+        }
+        if(e[0]==10){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador10: e[1] })
+        }
+        if(e[0]==11){
+            const docRef = await updateDoc(doc(db, 'campeonato25', 'fecha8'), { jugador11: e[1] })
         }
     }
 
@@ -117,7 +189,7 @@ export default function Paasmal(){
                         <PartidosSemana
                             title='Partidos de la semana:'
                             fecha={fecha}
-                            control={[1, 2, 3, 4, 5, 6]}
+                            control={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]}
                             onStatus={handleStatus}
                             onGoles={handleGoles}
                             onAgregar={handleAgregar}
