cba98619b2f5cefd8ef665cccd15e5bab3c04211
paasmal <pedroasmal@hotmail.com>
2025-04-23 12:32:12 -0500

    version 1.0.53

--- componentes/campeonato25/calendario/PartidosSemana.js
+++ componentes/campeonato25/calendario/PartidosSemana.js
@@ -63,17 +63,42 @@ export default function PartidosSemana({
             />
             { title2 && <p>{title2}</p> }
             <ItemCalendario
-                com
-                fase='Semifinal - Comunión'
-                control={control && control[0]}
-                now={fecha?.partido1}
-                res={fecha?.res1}
-                jugador={fecha?.jugador1}
-                pen={fecha?.pen1}
+                fase='Para semifinal'
+                control={control && control[2]}
+                now={fecha?.partido3}
+                res={fecha?.res3}
+                jugador={fecha?.jugador3}
+                pen={fecha?.pen3}
+                extra={fecha?.extra3}
                 home={home}
                 dia='Domingo'
                 fecha='20 Abr'
                 hora='09h30'
+                genero='M'
+                equipos={['Juntos con Cristo', 'Los Angelicales']}
+                paralelos={['AB 1C Ángeles (M)', 'AB 1C Molinopamba (M)']}
+                logos={['AAM', 'AMM']}
+                colores={{
+                    eq1: ['white', 'black', 'black'],
+                    eq2: ['white', 'black', 'black']
+                }}
+                onStatus={onStatus}
+                onGoles={onGoles}
+                onAgregar={onAgregar}
+            />
+            <ItemCalendario
+                com
+                fase='Semifinal - Comunión'
+                control={control && control[3]}
+                now={fecha?.partido4}
+                res={fecha?.res4}
+                jugador={fecha?.jugador4}
+                pen={fecha?.pen4}
+                extra={fecha?.extra4}
+                home={home}
+                dia='Domingo'
+                fecha='20 Abr'
+                hora='10h10'
                 genero='M'
                 equipos={['Leones de Dios', 'Guerreros contra el mal']}
                 paralelos={['2 Comun. 3 (M)', '2 Comun. 4 (M)']}
@@ -87,17 +112,41 @@ export default function PartidosSemana({
                 onAgregar={onAgregar}
             />
             <ItemCalendario
-                fase='Semifinal - Confirmación'
-                control={control && control[2]}
-                now={fecha?.partido3}
-                res={fecha?.res3}
-                jugador={fecha?.jugador3}
-                pen={fecha?.pen3}
-                extra={fecha?.extra3}
+                fase='Para semifinal'
+                control={control && control[4]}
+                now={fecha?.partido5}
+                res={fecha?.res5}
+                jugador={fecha?.jugador5}
+                pen={fecha?.pen5}
+                extra={fecha?.extra5}
                 home={home}
                 dia='Domingo'
                 fecha='20 Abr'
-                hora='10h30'
+                hora='10h50'
+                genero='M'
+                equipos={['Los Misioneros', 'Equipo de Dios']}
+                paralelos={['Año Bíblico 2 (M)', 'Año Bíblico 4 (M)']}
+                logos={['A2M', 'A4M']}
+                colores={{
+                    eq1: ['white', 'black', 'black'],
+                    eq2: ['red', 'red', 'white']
+                }}
+                onStatus={onStatus}
+                onGoles={onGoles}
+                onAgregar={onAgregar}
+            />
+            <ItemCalendario
+                fase='Semifinal - Confirmación'
+                control={control && control[5]}
+                now={fecha?.partido6}
+                res={fecha?.res6}
+                jugador={fecha?.jugador6}
+                pen={fecha?.pen6}
+                extra={fecha?.extra6}
+                home={home}
+                dia='Domingo'
+                fecha='20 Abr'
+                hora='11h30'
                 genero='F'
                 equipos={['Confirmación FC', 'Estrellitas de María']}
                 paralelos={['2 Confir. 2 (F)', '2 Confir. Ángeles (F)']}
@@ -113,15 +162,16 @@ export default function PartidosSemana({
             <ItemCalendario
                 com
                 fase='Semifinal - Comunión'
-                control={control && control[0]}
-                now={fecha?.partido1}
-                res={fecha?.res1}
-                jugador={fecha?.jugador1}
-                pen={fecha?.pen1}
+                control={control && control[6]}
+                now={fecha?.partido7}
+                res={fecha?.res7}
+                jugador={fecha?.jugador7}
+                pen={fecha?.pen7}
+                extra={fecha?.extra7}
                 home={home}
                 dia='Domingo'
                 fecha='20 Abr'
-                hora='11h30'
+                hora='12h20'
                 genero='F'
                 equipos={['Pueblo de Israel', 'El Maná']}
                 paralelos={['2 Comun. 1 (F)', '2 Comun. 5 (F)']}
@@ -136,16 +186,16 @@ export default function PartidosSemana({
             />
             <ItemCalendario
                 fase='Semifinal - Confirmación'
-                control={control && control[1]}
-                now={fecha?.partido2}
-                res={fecha?.res2}
-                jugador={fecha?.jugador2}
-                pen={fecha?.pen2}
-                extra={fecha?.extra2}
+                control={control && control[7]}
+                now={fecha?.partido8}
+                res={fecha?.res8}
+                jugador={fecha?.jugador8}
+                pen={fecha?.pen8}
+                extra={fecha?.extra8}
                 home={home}
                 dia='Domingo'
                 fecha='20 Abr'
-                hora='12h30'
+                hora='13h00'
                 genero='M'
                 equipos={['Los Ángeles Negros', 'Confirmación FC']}
                 paralelos={['1 Confir. 3 (M)', '2 Confir. 2 (M)']}
