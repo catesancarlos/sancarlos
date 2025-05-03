e8c0c339acd2dc1bf0217d5129056d0e0a6c1db7
paasmal <pedroasmal@hotmail.com>
2025-05-03 10:31:35 -0500

    version 1.0.60

--- pages/jesuscribe/index.js
+++ pages/jesuscribe/index.js
@@ -1,106 +1,67 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
 
-import AppLayout from '../../componentes/layout'
-import Container from '../../componentes/sections/Container'
-import InvitacionJs25 from '../../componentes/jesuscribe/InvitacionJs25'
-import CategoriasJs25 from '../../componentes/jesuscribe/CategoriasJs25'
-import FechasJs25 from '../../componentes/jesuscribe/FechasJs2025'
+import JsLayout from '../../componentes/layout/JsLayout'
 
 export default function Jesuscribe(){
     const router = useRouter()
     const [section, setSection] = useState(1)
 
-    useEffect(() => {
-        if(router.query.s){
-            setSection(router.query.s)
-            router.replace('/jesuscribe')
-        }   
-    }, [router.query.s])
-
     return(
-        <AppLayout titulo='San Carlos - Jesuscribe' name='Jesuscribe'>
-            <Container
-                title='CONCURSO'
-                subtitle='2025'
-                img='/logo-jesuscribe.jpg'
-            >
-                <aside>
-                    <p className={`op-menu ${section == 1 && 'active'}`} onClick={() => setSection(1)}>Invitación</p>
-                    <p className={`op-menu ${section == 3 && 'active'}`} onClick={() => setSection(3)}>Fechas</p>
-                    <p className={`op-menu ${section == 2 && 'active'}`} onClick={() => setSection(2)}>Niveles</p>
-                    <p className={`op-menu ${section == 4 && 'active'}`} onClick={() => {setSection(0), router.push('/jesuscribe/js-general')}}>General</p>
-                    <p className={`op-menu ${section == 5 && 'active'}`} onClick={() => {setSection(0), router.push('/jesuscribe/js-vida')}}>Vida de Jesús</p>
-                    <p className={`op-menu ${section == 6 && 'active'}`} onClick={() => {setSection(0), router.push('/jesuscribe/js-iglesia')}}>Nuestra Iglesia</p>
-                    <p className={`op-menu ${section == 7 && 'active'}`} onClick={() => {setSection(0), router.push('/jesuscribe/js-sacramentologia')}}>Sacramentologia</p>
-                    <p className={`op-menu ${section == 8 && 'active'}`} onClick={() => {setSection(0), router.push('/jesuscribe/js-liturgia')}}>Liturgia</p>
-                    <p className={`op-menu ${section == 9 && 'active'}`} onClick={() => {setSection(0), router.push('/jesuscribe/js-multiple')}}>Multiple</p>
-                </aside>
-                <div className='principal'>
-                    {
-                        section == 1 ? <InvitacionJs25 /> : 
-                            section == 2 ? <CategoriasJs25 /> :
-                                section == 3 ? <FechasJs25 /> : ''
-                    }
-                </div>
-            </Container>
+        <JsLayout 
+            titulo='JESUSCRIBE'
+            name='Jesuscribe'
+            back='#B0C0D9'
+            preg
+        >
+            <div className='cont'>
+                <p className='info'>Categorias:</p>
+                <p className='cat' onClick={() => router.push('/jesuscribe/gn')}>GENERAL</p>
+                <p className='cat' onClick={() => router.push('/jesuscribe/vd')}>VIDA DE JESÚS</p>
+                <p className='cat' onClick={() => router.push('/jesuscribe/ni')}>NUESTRA IGLESIA</p>
+                <p className='cat' onClick={() => router.push('/jesuscribe/lt')}>LITURGIA</p>
+                {/* <p className='cat' onClick={() => router.push('/jesuscribe/sc')}>SACRAMENTOLOGIA</p> */}
+                <p className='cat' onClick={() => router.push('/jesuscribe/ml')}>MULTIPLE</p>
+            </div>
 
             <style jsx>{`
-                aside{
-                    width: 15%;
+                .cont{
+                    margin: 36px 10% 0 10%;
+                    width: 80%;
                 }
 
-                .op-menu{
+                .info{
+                    font-size: 28px;
+                    font-family: 'Lato', sans-serif;
+                    font-weight: bold;
+                }
+
+                .cat{
+                    margin-top: 25px;
                     background: white;
                     padding: 10px 16px;
-                    font-size: 18px;
+                    font-size: 24px;
                     font-family: 'Lato', sans-serif;
                     text-align: center;
                     display: flex;
                     justify-content: center;
                     align-items: center;
-                    border-radius: 8px;
+                    border-radius: 15px;
                     box-shadow: -2px 2px 5px 0px #888;
                     -webkit-tap-highlight-color: rgba(0,0,0,0);
+                    transition: 0.5s ease;
                     cursor: pointer;
-                    margin-bottom: 12px;
                 }
 
-                .active{
-                    background: #EE1C21;
+                .cat:hover{
+                    background: black;
                     color: white;
                 }
 
-                .principal{
-                    width: 85%;
-                    padding: 18px 0 18px 35px;
-                }
-
                 @media screen and (max-width: 768px){
-                    aside{
-                        width: 100%;
-                        display: flex;
-                        flex-wrap: wrap;
-                    }
-
-                    .op-menu{
-                        width: calc((100% - 12px) / 3);
-                        padding: 6px 5px;
-                        font-size: 15px;
-                        margin-right: 6px;
-                        margin-bottom: 10px;
-                    }
-
-                    .op-menu:nth-child(3), .op-menu:nth-child(6), .op-menu:nth-child(9), .op-menu:nth-child(10){
-                        margin-right: 0;
-                    }
-
-                    .principal{
-                        width: 100%;
-                        padding: 15px 0 0 0;
-                    }
+                    
                 }
             `}</style>
-        </AppLayout>
+        </JsLayout>
     )
 }
