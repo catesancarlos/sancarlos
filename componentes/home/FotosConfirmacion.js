ad4f8a349bf62ff2c2ee99d87ef41703c99e075d
catesancarlos <catesancarlosricaurte@gmail.com>
2025-06-07 00:28:30 -0500

    version 1.0.72

--- componentes/home/FotosConfirmacion.js
+++ componentes/home/FotosConfirmacion.js
@@ -11,19 +11,19 @@ const FotosConfirmacion = () => {
             <div className='cont c1'>
                 <div
                     className='opcion'
-                    onClick={() => router.push('https://www.dropbox.com/scl/fo/pzyhirhoq6t993da28x87/AGdhmttKxufRc4FqqkQlU_I?rlkey=3iyivhppqw0m666i33f9av0cc&st=qz4fw17o&dl=0')}>
+                    onClick={() => router.push('https://www.dropbox.com/scl/fo/qv05nowpkr8phlbmvo66i/AKVnpkDj9a06kc2pQOE_mZQ?rlkey=k4oybs5vie8vzo6iu0bfykcrm&st=zjm69y5t&dl=0')}>
                     <strong>Eucaristia 8am</strong>
                     <p>Ingreso</p>
                 </div>
                 <div
                     className='opcion'
-                    onClick={() => router.push('https://www.dropbox.com/scl/fo/pzyhirhoq6t993da28x87/AGdhmttKxufRc4FqqkQlU_I?rlkey=3iyivhppqw0m666i33f9av0cc&st=qz4fw17o&dl=0')}>
+                    onClick={() => router.push('https://www.dropbox.com/scl/fo/6qu9lj4mrch5t5oo7mwop/ALO9o2f2qaQdw-FmTlOb9VM?rlkey=drtjhmvf26bfq7ya1ay1h24pg&st=b5mhgf3u&dl=0')}>
                     <strong>Eucaristia 8am</strong>
                     <p>Unción Crisma</p>
                 </div>
                 <div
                     className='opcion'
-                    onClick={() => router.push('https://www.dropbox.com/scl/fo/pzyhirhoq6t993da28x87/AGdhmttKxufRc4FqqkQlU_I?rlkey=3iyivhppqw0m666i33f9av0cc&st=qz4fw17o&dl=0')}>
+                    onClick={() => router.push('https://www.dropbox.com/scl/fo/cxrcd7wfk2w8cea1m0qap/AIrz2VeiXKLYuiyBwxF4poI?rlkey=axgj4qiejholwbw9wt36xxn15&st=slepulk9&dl=0')}>
                     <strong>Eucaristia 8am</strong>
                     <p>Varias</p>
                 </div>
@@ -31,19 +31,19 @@ const FotosConfirmacion = () => {
             <div className='cont c2'>
                 <div
                     className='opcion'
-                    onClick={() => router.push('https://www.dropbox.com/scl/fo/18rcp8dqouh2yz5d3ndve/ADGIyLet2DrrtK-Ds4Mvyr4?rlkey=xkpwykgwf9k026799qee4lb48&st=ijtlszdp&dl=0')}>
+                    onClick={() => router.push('https://www.dropbox.com/scl/fo/besb4xnobva1e7nsg102e/AKv2PNIOTGR7rVPuStpcobA?rlkey=1k1u1gd5v9xvddvy7dch36pug&st=tz6is6ku&dl=0')}>
                     <strong>Eucaristia 10am</strong>
                     <p>Ingreso</p>
                 </div> 
                 <div
                     className='opcion'
-                    onClick={() => router.push('https://www.dropbox.com/scl/fo/18rcp8dqouh2yz5d3ndve/ADGIyLet2DrrtK-Ds4Mvyr4?rlkey=xkpwykgwf9k026799qee4lb48&st=ijtlszdp&dl=0')}>
+                    onClick={() => router.push('https://www.dropbox.com/scl/fo/1d1xazwfngblyfjgihjwr/AJpj6BaRT3NKl-sUYU74B2o?rlkey=1qh89rce5bk30fb4g3kv12xxu&st=fw45twf3&dl=0')}>
                     <strong>Eucaristia 10am</strong>
                     <p>Unción Crisma</p>
                 </div>
                 <div
                     className='opcion'
-                    onClick={() => router.push('https://www.dropbox.com/scl/fo/18rcp8dqouh2yz5d3ndve/ADGIyLet2DrrtK-Ds4Mvyr4?rlkey=xkpwykgwf9k026799qee4lb48&st=ijtlszdp&dl=0')}>
+                    onClick={() => router.push('https://www.dropbox.com/scl/fo/uhfg1ogymnvctyrtxf57l/ADdPhdw_VlJ4Z8lJe75LNIw?rlkey=mbmqauhdsiuyqcv93wong1po6&st=t692nn20&dl=0')}>
                     <strong>Eucaristia 10am</strong>
                     <p>Varias</p>
                 </div>       
